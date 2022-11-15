4a23dd4ddb669b6f4e96fd23c3bcb2fc88fb4d3f
soha <sousarit@sheridancollege.ca>
2022-11-15 17:07:57 -0500

    add placeholder dialog

---
+++ TeamMAs_Project/Assets/DialogStarter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class DialogStarter : MonoBehaviour
+{
+  [SerializeField] string conversation; // the title of the conversation
+  
+  void Start()
+  {
+    StartCoroutine(DialogTest(1f));
+  }
+  
+  IEnumerator DialogTest(float delayTime) {
+    yield return new WaitForSeconds(delayTime);
+    //DialogueManager.StartConversation(string conversation, Transform actor, Transform conversant); // actor and conversant are optional
+    DialogueManager.StartConversation(conversation);
+    //GetComponent<DialogueSystemTrigger>().OnUse();  // also works, only if using a DialogueSystemTrigger component set to OnUse
+  }
+}
