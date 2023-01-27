6ed63ed03652028e49bf893c783bb06cf1f9eb6a
soha <sousarit@sheridancollege.ca>
2023-01-27 02:26:27 -0500

    edit coin UI, add water dialog

--- TeamMAs_Project/Assets/Source/SaritasScripts/Dialog.cs
+++ TeamMAs_Project/Assets/Source/SaritasScripts/Dialog.cs
@@ -4,27 +4,43 @@ using PixelCrushers.DialogueSystem;
 
 public class Dialog : MonoBehaviour
 {
-  [SerializeField] string conversation; // the title of the conversation
-  
-  /* NOTES
-  Using Names in Dialog Text
-  Player Name: [lua(Actor["Player"].Display_Name)]
-  Actor Name: [var=Actor]
-  */
-  
-  void Start()
-  {
-    StartCoroutine(DialogTest(1f));
-  }
-  
-  IEnumerator DialogTest(float delayTime) {
-    yield return new WaitForSeconds(delayTime);
-    //DialogueManager.StartConversation(string conversation, Transform actor, Transform conversant); // actor and conversant are optional
-    DialogueManager.StartConversation(conversation);
-    //GetComponent<DialogueSystemTrigger>().OnUse();  // also works, only if using a DialogueSystemTrigger component set to OnUse
-  }
-  
-  public void SkipDialog(){
-    DialogueManager.StopConversation();
-  }
+    [SerializeField] string conversation; // the title of the conversation
+
+    /* NOTES
+    Using Names in Dialog Text
+    Player Name: [lua(Actor["Player"].Display_Name)]
+    Actor Name: [var=Actor]
+
+    Setting Portraits in Dialog Text
+    e.g. put
+    [pic=2]
+    anywhere in the dialog text to use the 2nd sprite
+
+    Italics
+    [em1]italicized text[/em1]
+    * customizable in Database -> Database Properties -> Emphasis Settings
+    */
+
+    void Start()
+    {
+        StartCoroutine(DialogTest(1f));
+    }
+
+    IEnumerator DialogTest(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        //DialogueManager.StartConversation(string conversation, Transform actor, Transform conversant); // actor and conversant are optional
+        DialogueManager.StartConversation(conversation);
+        //GetComponent<DialogueSystemTrigger>().OnUse();  // also works, only if using a DialogueSystemTrigger component set to OnUse
+    }
+
+    public void SkipDialog()
+    {
+        DialogueManager.StopConversation();
+    }
+
+    public void StartConversation(string conversation)
+    {
+        DialogueManager.StartConversation(conversation);
+    }
 }
